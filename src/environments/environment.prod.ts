8746ac55939d4b74e2c6e10ac8f1c3b8888954ca
The Anh Nguyen <anhnguyenthe29112004@gmail.com>
2025-06-15 18:03:00 +0700

    fix: Update API URL to correct production endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://157.230.253.159/api' // Thay đổi URL này thành URL API production thực tế của bạn
+    apiUrl: 'https://loanconv.me/api'  // Thay đổi URL này thành URL API production thực tế của bạn
 };
